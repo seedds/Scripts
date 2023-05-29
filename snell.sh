7a5dc4c3edae4001b83356fbca89f0ea8311c17e
seedds <seedds@gmail.com>
2023-05-30 00:41:56 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -3,6 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
+SHADOW_TLS_SERVICE="/etc/systemd/system/shadow-tls.service"
 apt-get install unzip -y
 cd ~/
 wget --no-check-certificate -O snell.zip https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip
@@ -63,7 +64,6 @@ systemctl start snell
 
 wget --no-check-certificate https://github.com/ihciah/shadow-tls/releases/download/v0.2.23/shadow-tls-x86_64-unknown-linux-musl -O /usr/local/bin/shadow-tls
 chmod +x /usr/local/bin/shadow-tls
-SHADOW_TLS_SERVICE = "/etc/systemd/system/shadow-tls.service"
 rm ${SHADOW_TLS_SERVICE}
 touch ${SHADOW_TLS_SERVICE}
 echo "[Unit]" >> ${SHADOW_TLS_SERVICE}
